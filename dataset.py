79e5d19afa9a962c0461485f623fad0cbffbc99c
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-10-17 16:22:21 -0400

    Wrote helpful comments

--- dataset.py
+++ dataset.py
@@ -11,11 +11,12 @@ labels_tensor = torch.tensor(labels, dtype=torch.long)
 dataset = TensorDataset(data_tensor, labels_tensor)
 
 num_samples = len(dataset)
-train_size = int(0.8 * num_samples)
+train_size = int(0.8 * num_samples) # split is 80% training and 20% test
 test_size = num_samples - train_size
 
-train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size]) # random split
 
+# batches of data to feed the model
 batch_size = 32
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True) 
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
