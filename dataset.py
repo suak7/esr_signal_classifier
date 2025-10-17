c9f38cd9bb2eb51adb848aaf1ff8b772209e3ed1
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-10-17 16:19:45 -0400

    Created training and test sets

---
+++ dataset.py
@@ -0,0 +1,21 @@
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, random_split
+from torch.utils.data import DataLoader
+
+data = np.load("signals.npy")
+labels = np.load("labels.npy")
+data_tensor = torch.tensor(data, dtype=torch.long)
+labels_tensor = torch.tensor(labels, dtype=torch.long)
+
+dataset = TensorDataset(data_tensor, labels_tensor)
+
+num_samples = len(dataset)
+train_size = int(0.8 * num_samples)
+test_size = num_samples - train_size
+
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+batch_size = 32
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
