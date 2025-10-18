f9268450dc85ec15b41f1c30b5f3d7c96f07253c
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-10-18 11:20:08 -0400

    Defined class and forward method for linear layers

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class SignalClassifier(nn.Module): # linear layers with relu activations
+    def __init__(self):
+        super(SignalClassifier, self).__init__()
+        self.linear1 = nn.Linear(300, 128)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(128, 64)
+        self.relu = nn.ReLU()
+        self.linear3 = nn.Linear(64, 3)
+    
+    def forward(self, x): 
+        x = self.linear1(x)
+        x = self.relu(x)  
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
+        return x
