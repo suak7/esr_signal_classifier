48fd0bf052d5cb1c72a128ee1209ab0c7e0935e7
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-10-18 17:12:05 -0400

    Added comments

--- model.py
+++ model.py
@@ -1,23 +1,27 @@
-import torch
 import torch.nn as nn
-class SignalClassifier(nn.Module): # linear layers with dropouts
+class SignalClassifier(nn.Module): 
     def __init__(self):
         super(SignalClassifier, self).__init__()
         self.linear1 = nn.Linear(1000, 256)
         self.relu = nn.ReLU()
-        self.dropout1 = nn.Dropout(0.2)  
+        self.dropout1 = nn.Dropout(0.2) # dropout to prevent overfitting (20% of neurons dropped)
+        
         self.linear2 = nn.Linear(256, 128)
         self.dropout2 = nn.Dropout(0.2)  
+        
         self.linear3 = nn.Linear(128, 3)
     
     def forward(self, x):
+        # layer 1: linear --> relu --> dropout
         x = self.linear1(x)
         x = self.relu(x)
         x = self.dropout1(x)
 
+        # layer 1: linear --> relu --> dropout
         x = self.linear2(x)
         x = self.relu(x)
         x = self.dropout2(x)
 
+        # output layer: raw scores for 3 classes
         x = self.linear3(x)
         return x
